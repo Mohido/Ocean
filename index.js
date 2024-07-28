305f5420187dcc43d30be4173e55e93296e05f4c
Mohammed Al-Mahdawi <m.ghaithalmahdawi@gmail.com>
2024-07-28 06:22:19 +0200

    Shifted Wave Vertices

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { VertexNormalsHelper } from 'three/addons/helpers/VertexNormalsHelper.js';
 import { PMREMGenerator } from 'three/extras/PMREMGenerator.js';
 import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
 
@@ -11,18 +10,48 @@ const projObjects = {
     controls: undefined,        // controls three object
     renderer: undefined,        // Renderer Three Object
     hdri : undefined,           // Three texture (gotten by rgbeloader)
+    waves : [],
+    waveMat : undefined
 }
-
 const wireframeMode = true;
 
-const waves = [
-    {
-        dir : [0.0, 1.0],
-        speed : 1.0,
-        amplitude : 1.0,
-        length : 4
+
+// Function to rotate a 2D vector
+const rotateVector2 = (vector, angle) => {
+    const cos = Math.cos(angle);
+    const sin = Math.sin(angle);
+    const x = vector.x * cos - vector.y * sin;
+    const y = vector.x * sin + vector.y * cos;
+    return new THREE.Vector2(x, y);
+}
+
+const initWaves = () => {
+    projObjects.waves = [];         // Clear the waves
+    const medianLength = 4.0;       // the length of the median wave. We will create waves randomly from half to double this median
+    const ampToLengthRatio = 1/6;   // The ratio of the amplitude to wavelength. The amplitude must correspond to teh length for a abetter control
+    const numOfWaves = 4;           // number of waves we need to create.
+    // Wind Direction. Waves will be randomly created with this direction with some tilt
+    const wind = new THREE.Vector2(0,1);
+    const gravity = 10;             // 10m/s2
+    const windMaxAngle = Math.PI/4; // Generating random winds tilted with this angle
+    
+    
+    for(let i = 0 ; i < numOfWaves; i++){
+        const wlen = medianLength/2 + Math.random()*(medianLength*2 - medianLength/2);
+        const rad =  windMaxAngle * (Math.random()*2 - 1);      // Between -windMaxAngle and windMaxAngle rotation angle
+        const dir = rotateVector2(wind, rad).setLength(1);
+        projObjects.waves.push(
+            {
+                dir :  [dir.x, dir.y],
+                speed : Math.sqrt(gravity * 2 * Math.PI / wlen),        // Speed in how many units per second does the wave move
+                amplitude : wlen*ampToLengthRatio,
+                length : wlen,
+                steepness : Math.random()   // Random steepness
+            }
+        );
     }
-]
+}
+
 
 
 const initRenderer = () => {
@@ -39,7 +68,6 @@ const initCamera = () => {
 }
 
 
-
 const initBackground = async () => {
     const rgbeLoader = new RGBELoader();
     projObjects.hdri = await rgbeLoader.loadAsync('./public/kloppenheim_06_puresky_1k.hdr');
@@ -52,18 +80,19 @@ const initBackground = async () => {
 
 const initOcean = () => {
     const pmremGenerator = new PMREMGenerator(projObjects.renderer);
-    const envMap = pmremGenerator.fromEquirectangular(projObjects.hdri).texture;
+    const envMap = pmremGenerator.fromEquirectangular(projObjects.hdri).texture.clone();
     pmremGenerator.dispose();
 
-    const myShaderMaterial = new THREE.ShaderMaterial( {
+    projObjects.waveMat = new THREE.ShaderMaterial( {
         uniforms: {
             time : {value : 1.0},
             
-            WDirs : {value: waves.flatMap((value) => value.dir)},
-            WSpeeds : {value: waves.flatMap((value) => value.speed)},
-            WAmplitudes : {value: waves.flatMap((value) => value.amplitude)},
-            WLengths : {value: waves.flatMap((value) => value.length)},
-
+            WDirs : {value: projObjects.waves.flatMap((value) => value.dir)},
+            WSpeeds : {value:  projObjects.waves.flatMap((value) => value.speed)},
+            WAmplitudes : {value:  projObjects.waves.flatMap((value) => value.amplitude)},
+            WLengths : {value:  projObjects.waves.flatMap((value) => value.length)},
+            WSteep : {value :  projObjects.waves.flatMap((value) => value.steepness)},
+            
             envMap: { value: envMap },
             reflective: {value : 1.0},
             color : {value : new THREE.Vector3(1,1,1)}
@@ -73,16 +102,40 @@ const initOcean = () => {
             varying vec3 vPosition;
 
             uniform float time;          // animation time
-            uniform float WDirs[${2*waves.length}];      // wave direction
-            uniform float WSpeeds[${waves.length}];
-            uniform float WAmplitudes[${waves.length}];
-            uniform float WLengths[${waves.length}];
+            uniform float WDirs[${2 * projObjects.waves.length}];      // wave direction
+            uniform float WSpeeds[${ projObjects.waves.length}];
+            uniform float WAmplitudes[${ projObjects.waves.length}];
+            uniform float WLengths[${ projObjects.waves.length}];
+            uniform float WSteep[${ projObjects.waves.length}];
             
-            void main() {        
+            #define PI2 6.28318530718
+
+            void main() {      
+                vec3 nPos = vec3(position.xy,0.0);
+
+                for(int i = 0 ; i < ${ projObjects.waves.length} ; i++){
+                    // Pis in a cycle
+                    float pis_cycle = WLengths[i] / PI2;
+                    // Vertices on the same line are projected
+                    vec2 dir = normalize(vec2(WDirs[i*2], WDirs[i*2+1]));
+                    float dp = dot(dir, position.xy);
+                    // frequency of the wave
+                    float f = PI2/WLengths[i];
+                    // phase = speed*frequney*time
+                    float p = WSpeeds[i] * f * time;
+                    // wave cos
+                    float wcos = cos(f*dp + p);
+
+                    nPos.x += (WSteep[i] /${ projObjects.waves.length}.0) * pis_cycle * WDirs[i*2] * wcos;
+                    nPos.y += (WSteep[i] /${ projObjects.waves.length}.0) * pis_cycle * WDirs[i*2+1] * wcos;
+                    nPos.z = WAmplitudes[i] * sin(dp*f + p);
+                }
+
                 // Create the normal matrix in the vertex shader
                 vNormal = transpose(inverse(mat3(modelMatrix))) * normal;
                 vPosition = vec3(modelMatrix * vec4(position, 1.0));
-                gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+                // gl_Position = projectionMatrix * modelViewMatrix * vec4(position.xyz, 1.0);
+                gl_Position = projectionMatrix * modelViewMatrix * vec4(nPos.xyz, 1.0);
             }
         `,
         fragmentShader: `
@@ -91,11 +144,11 @@ const initOcean = () => {
             uniform float reflective;
 
             // waves data:
-            uniform float WDirs[${2*waves.length}];      // wave direction
-            uniform float WSpeeds[${waves.length}];
-            uniform float WAmplitudes[${waves.length}];
-            uniform float WLengths[${waves.length}];
-
+            uniform float WDirs[${2* projObjects.waves.length}];      // wave direction
+            uniform float WSpeeds[${ projObjects.waves.length}];
+            uniform float WAmplitudes[${ projObjects.waves.length}];
+            uniform float WLengths[${ projObjects.waves.length}];
+            uniform float WSteep[${ projObjects.waves.length}];
 
             varying vec3 vPosition;
             varying vec3 vNormal;
@@ -105,10 +158,11 @@ const initOcean = () => {
                     gl_FragColor = vec4(color, 1.0); 
                     return;
                 }
-
+                
                 vec3 I = normalize(vPosition - cameraPosition);
                 vec3 R = reflect(I, normalize(vNormal));
                 vec3 envColor = textureCube(envMap, R).rgb;
+            
                 gl_FragColor = vec4(envColor, 1.0);
             }
         `,
@@ -116,15 +170,15 @@ const initOcean = () => {
     } );
     
     // Objects Initializations
-    const geometry = new THREE.PlaneGeometry(20, 20, 40, 40);
-    const plane = new THREE.Mesh( geometry, myShaderMaterial );
+    const geometry = new THREE.PlaneGeometry(100, 100, 100, 100);
+    const plane = new THREE.Mesh( geometry, projObjects.waveMat );
     plane.rotation.x = -Math.PI/2;
 
     // Creating Wireframe
     if(wireframeMode){
         const wireframe = new THREE.WireframeGeometry( plane.geometry );
-        myShaderMaterial.uniforms.reflective.value = 0.0;
-        const line = new THREE.LineSegments( wireframe , myShaderMaterial);
+        projObjects.waveMat.uniforms.reflective.value = 0.0;
+        const line = new THREE.LineSegments( wireframe , projObjects.waveMat);
         line.material.depthTest = false;
         line.material.opacity = 0.25;
         line.material.transparent = true;
@@ -140,6 +194,7 @@ const initScene = async () =>{
     // Engine Initialization
     projObjects.scene = new THREE.Scene();
     await initBackground();
+    initWaves();
     initOcean();
 }
 
@@ -154,6 +209,7 @@ const init = async () => {
 
 const draw = () => {
     const animate = () => {
+        projObjects.waveMat.uniforms.time.value = performance.now() / 2000; // Time in seconds
         projObjects.controls.update();
         projObjects.renderer.render( projObjects.scene, projObjects.camera );
         projObjects.renderer.setAnimationLoop( animate );
